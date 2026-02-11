68eeb5cf8c3baba8c669fcb8ed18decaac0e5304
Deepanshu Chaid <bantar117@gmail.com>
2026-02-11 21:04:32 +0000

    *validator

--- internal/types/types.go
+++ internal/types/types.go
@@ -4,5 +4,5 @@ type Todo struct {
   Id int64 `json:"id"`
   Title string `json:"title" validate:"required"`
   Description string `json:"description" validate:"required"`
-  Completed bool `json:"completed" validate:"required"`
+  Completed bool `json:"completed"`
 }
